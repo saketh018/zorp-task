02e4f14e3f7cc6122f85f281d681833dd9c58e9b
saketh018 <saketh50@gmail.com>
2021-05-25 23:15:57 +0530

    basic  map setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React,{useState,useEffect} from "react";
+import {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+function Map() {
+const [data,setData]=useState([]);
+
+
+// const retrieveData= async()=>
+// {
+//   const response = await axios.get("http://ec2-13-126-90-72.ap-south-1.compute.amazonaws.com:8082/user/1/tasks/").then();
+//   return response.data;
+// }
+//  useEffect (()=>{
+// const getAllData = async ()=>
+// {
+//   const allData=await retrieveData();
+//   setData(allData);
+// };
+// getAllData();
+//  },[data]);
+ useEffect(async () => {
+  const result = await axios(
+    `http://ec2-13-126-90-72.ap-south-1.compute.amazonaws.com:8082/user/1/tasks/`
+  );
+
+  setData(result.data);
+});
+    return (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
         >
-          Learn React
-        </a>
-      </header>
+          </GoogleMap>
+          
+  );
+}
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDafac2DZUblVP9asDxR8P7g22qME2smKE&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
-
-export default App;
