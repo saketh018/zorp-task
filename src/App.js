8d5a6adceb356a3abbdb9c46634f2f591ab0e4f5
saketh018 <saketh50@gmail.com>
2021-05-26 16:30:41 +0530

    added maps, center,display when clicked on marker

--- src/App.js
+++ src/App.js
@@ -1,57 +1,98 @@
+// import all the required packages and libraries
 import axios from "axios";
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
-
-  withGoogleMap,
-  withScriptjs,
   GoogleMap,
-  Marker
-} from "react-google-maps";
+  Marker,
+  InfoWindow,
+  LoadScript,
+} from "@react-google-maps/api";
 
-function Map() {
-const [data,setData]=useState([]);
+//used the set state to store data from api
+const Map = () => {
+  const [dataX, setDataX] = useState([]);
+  const [selectedloc, setSelectedloc] = useState(null); // for the selected marker
 
-
-// const retrieveData= async()=>
-// {
-//   const response = await axios.get("http://ec2-13-126-90-72.ap-south-1.compute.amazonaws.com:8082/user/1/tasks/").then();
-//   return response.data;
-// }
-//  useEffect (()=>{
-// const getAllData = async ()=>
-// {
-//   const allData=await retrieveData();
-//   setData(allData);
-// };
-// getAllData();
-//  },[data]);
- useEffect(async () => {
-  const result = await axios(
-    `http://ec2-13-126-90-72.ap-south-1.compute.amazonaws.com:8082/user/1/tasks/`
-  );
-
-  setData(result.data);
-});
-    return (
-      <GoogleMap
-        defaultZoom={10}
-        defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-        >
-          </GoogleMap>
-          
-  );
-}
-const MapWrapped = withScriptjs(withGoogleMap(Map));
-
-export default function App() {
+  // fetching the data from the api and storing it locally
+  const retrieveData = () => {
+    const response = axios
+      .get(
+        "http://ec2-13-126-90-72.ap-south-1.compute.amazonaws.com:8082/user/1/tasks/"
+      )
+      .then((resp) => {
+        setDataX(resp.data);
+        return resp.data;
+      })
+      .catch((err) => {
+        console.log("error: " + err);
+      });
+  };
+  //providing duumy data in the beginning
+  const center = {
+    lat: 28.7041,
+    lng: 77.1025,
+  };
+  //useeffect will tell it needs to do something after render
+  useEffect(() => {
+    retrieveData();
+  }, []);
+  //styling the maps
+  const containerStyle = {
+    width: "100%",
+    height: "100vh",
+  };
+  // for the center to be assigned to task 1
+  const changeCord = () => {
+    dataX.forEach(function (item) {
+      if (item.seq === 1) {
+        center.lat = item.location.lat;
+        center.lng = item.location.lon;
+      }
+    });
+  };
+  //map implementation and components for map
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDafac2DZUblVP9asDxR8P7g22qME2smKE&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyDafac2DZUblVP9asDxR8P7g22qME2smKE">
+      {changeCord()}
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={19}>
+        <>
+          {/* iterating over the data and assgining values using it */}
+          {dataX.map((item) => (
+            <Marker
+              key={item.taskId}
+              position={{ lat: item.location.lat, lng: item.location.lon }}
+              name={item.seq}
+              icon={{
+                url:
+                  "http://www.googlemapsmarkers.com/v1/" +
+                  item.seq +
+                  "/0099FF/",
+              }}
+              onClick={() => {
+                setSelectedloc(item);
+              }}
+            />
+          ))}
+          {/* in case we select particluar marker then to get the details about it.info window is being used to display the data */}
+          {selectedloc && (
+            <InfoWindow
+              onCloseClick={() => {
+                setSelectedloc(null);
+              }}
+              position={{
+                lat: selectedloc.location.lat,
+                lng: selectedloc.location.lon,
+              }}
+            >
+              <div>
+                <h2>{selectedloc.name}</h2>
+                <p>{selectedloc.customerInfo}</p>
+              </div>
+            </InfoWindow>
+          )}
+        </>
+      </GoogleMap>
+    </LoadScript>
   );
-}
+};
+export default React.memo(Map);
